1059b3de360f90092794fe0aad87546b50523b44
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-26 16:09:46 -0400

    Seperated search bar into its own component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import SearchBar from "./SearchBar";
+
 export default function Navbar() {
     return (
         <div className="w-full fixed top-0 border-b-2 border-gray-700 bg-gray-900">
@@ -13,48 +15,11 @@ export default function Navbar() {
                     <span className="ml-2 text-3xl font-semibold text-gray-200">
                         Novel Opinions
                     </span>
-                    <div className="w-9/12 h-full flex items-center">
-                        <form className="w-9/12 mx-auto">
-                            <label
-                                htmlFor="search"
-                                className="mb-2 text-sm font-medium text-gray-900 sr-only text-white"
-                            >
-                                Search
-                            </label>
-                            <div className="relative">
-                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                    <svg
-                                        aria-hidden="true"
-                                        className="w-5 h-5 text-gray-500"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                        ></path>
-                                    </svg>
-                                </div>
-                                <input
-                                    type="search"
-                                    id="search"
-                                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                                    placeholder="Search books..."
-                                    required
-                                />
-                                <button
-                                    type="submit"
-                                    className="absolute right-2.5 bottom-2.5 focus:outline-none rounded-lg text-sm py-2 px-4 bg-gray-300 font-bold text-gray-800 hover:bg-gray-400"
-                                >
-                                    Search
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+
+                    {/*
+                        SEARCH BAR COMPONENT
+                    */}
+                    <SearchBar />
                 </div>
                 <div className="w-3/12 p-2">
                     <div className="w-full h-full flex justify-evenly items-center">
