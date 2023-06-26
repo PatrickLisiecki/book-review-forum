be5bb2d906051be66f11b9c53d8c1bd43ec241a3
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-26 16:08:24 -0400

    Added new books and genre attribute

--- src/books.js
+++ src/books.js
@@ -1,51 +1,57 @@
 const books = [
     {
         image: {
-            src: "src/images/book.png",
+            src: "https://i.weltbild.de/p/the-great-gatsby-310391702.jpg?v=1&wp=_ads-minzoom2",
             alt: "The Great Gatsby",
         },
         title: "The Great Gatsby",
         author: "F. Scott Fitzgerald",
+        genre: "fiction",
     },
     {
         image: {
-            src: "src/images/book.png",
-            alt: "The Great Gatsby",
+            src: "https://0.academia-photos.com/attachment_thumbnails/63753185/mini_magick20200626-26516-nmnvj5.png?1593237859",
+            alt: "1984",
         },
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
+        title: "1984",
+        author: "George Orwell",
+        genre: "fiction",
     },
     {
         image: {
-            src: "src/images/book.png",
-            alt: "The Great Gatsby",
+            src: "https://orion-uploads.openroadmedia.com/sm_9333ba5b72d2-hp.jpg",
+            alt: "Harry Potter and the Sorcerer's Stone",
         },
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K. Rowling",
+        genre: "fiction",
     },
     {
         image: {
-            src: "src/images/book.png",
-            alt: "The Great Gatsby",
+            src: "https://orion-uploads.openroadmedia.com/sm_f7e651-tolkien-lordoftherings.jpg",
+            alt: "Lord of the Rings",
         },
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
+        title: "Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        genre: "fiction",
     },
     {
         image: {
-            src: "src/images/book.png",
-            alt: "The Great Gatsby",
+            src: "https://d3i5mgdwi2ze58.cloudfront.net/rqf95utsbarxtxjfzgnypxkce5a3",
+            alt: "Lord of the Flies",
         },
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
+        title: "Lord of the Flies",
+        author: "William Golding",
+        genre: "fiction",
     },
     {
         image: {
-            src: "src/images/book.png",
-            alt: "The Great Gatsby",
+            src: "https://orion-uploads.openroadmedia.com/sm_2f32b6-catcher-in-the-rye-red.jpg",
+            alt: "The Catcher in the Rye",
         },
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
+        title: "The Catcher in the Rye",
+        author: "J. D. Salinger",
+        genre: "fiction",
     },
 ];
 
