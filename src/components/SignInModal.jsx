a8575f7500d60989d4318f24c2dcfe342b541a95
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-28 16:01:20 -0400

    Changed button styling

--- src/components/SignInModal.jsx
+++ src/components/SignInModal.jsx
@@ -52,7 +52,7 @@ export default function Modal({ isVisible, hideModal }) {
                         </fieldset>
 
                         <input
-                            className="bg-white hover:bg-gray-200 transition mt-4 px-4 py-2 border-2 border-gray-800 cursor-pointer text-gray-800"
+                            className="bg-gray-800 transition mt-4 px-4 py-2 rounded-xl border-2 border-gray-800 cursor-pointer text-white hover:bg-gray-700 hover:border-gray-700"
                             type="submit"
                             value="Log In"
                         />
