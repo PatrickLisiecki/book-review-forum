0d8e57e6676029392fd0a54199bcba616a66c8c7
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-28 22:04:51 -0400

    Added select element

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,17 +1,37 @@
 export default function AddBook({ showAddBookModal }) {
     return (
-        <div className="w-11/12 mx-auto my-10 flex flex-row gap-2 rounded-2xl p-5 bg-gray-600">
+        <div className="w-11/12 mx-auto my-10 flex flex-row justify-evenly rounded-2xl p-5 bg-gray-600">
             <button
                 onClick={showAddBookModal}
-                className="rounded-lg text-l py-2 px-4 bg-gray-300 font-bold text-gray-800 hover:bg-gray-400"
+                className="rounded-lg py-2 px-4 bg-gray-300 font-bold text-gray-800 hover:bg-gray-400"
             >
-                Add Book
+                Create a Forum
             </button>
-            <button
-                className="rounded-lg text-l py-2 px-4 bg-gray-300 font-bold text-gray-800 hover:bg-gray-400"
-            >
-                Genre
-            </button>
+            <div className="w-6/12 p-2 mx-auto flex justify-center items-center">
+                <label htmlFor="selectGenre" className="sr-only">
+                    Underline select
+                </label>
+                <select
+                    id="selectGenre"
+                    className="w-full block py-3 text-l text-gray-200 bg-transparent border-0 border-b-2 border-gray-200 focus:outline-none focus:ring-0 peer"
+                >
+                    <option className="text-gray-800" selected>
+                        Choose a genre
+                    </option>
+                    <option className="text-gray-800" value="fiction">
+                        Fiction
+                    </option>
+                    <option className="text-gray-800" value="nonFiction">
+                        Non-Fiction
+                    </option>
+                    <option className="text-gray-800" value="sciFi">
+                        Sci-Fi
+                    </option>
+                    <option className="text-gray-800" value="biography">
+                        Biography
+                    </option>
+                </select>
+            </div>
         </div>
     );
 }
