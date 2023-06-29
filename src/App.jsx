837525264a2a56c0ae469cfcd80b29f4f7bcc2a6
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-29 15:45:37 -0400

    Added react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Footer from "./components/Footer";
 import Modal from "./components/Modal";
 import "./assets/App.css";
 import AddBook from "./components/AddBook";
-import AddBookForm from "./components/AddBookForm";
+import CreateForumForm from "./components/CreateForumForm";
 import SignInForm from "./components/SignInForm";
 
 function App() {
     const [isSignInModalVisible, setIsSignInModalVisible] = useState(false);
-    const [isAddBookModalVisible, setIsAddBookModalVisible] = useState(false);
+    const [isCreateForumModalVisible, setIsCreateForumModalVisible] =
+        useState(false);
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
@@ -43,17 +44,17 @@ function App() {
         setIsSignInModalVisible(false);
     };
 
-    const showAddBookModal = () => {
-        setIsAddBookModalVisible(true);
+    const showCreateForumModal = () => {
+        setIsCreateForumModalVisible(true);
     };
 
-    const hideAddBookModal = () => {
-        setIsAddBookModalVisible(false);
+    const hideCreateForumModal = () => {
+        setIsCreateForumModalVisible(false);
     };
 
-    const onAddBook = (newBook) => {
+    const onCreateForum = (newBook) => {
         // modal should close
-        hideAddBookModal();
+        hideCreateForumModal();
 
         // new job should be added to the DOM
         setBooks((books) => {
@@ -65,18 +66,18 @@ function App() {
         <main className="w-full min-h-screen font-noto grid bg-gray-900">
             <Navbar books={books} showSignInModal={showSignInModal} />
 
-            <section className="mx-auto w-9/12 pt-32 flex-col justify-center items-center border-r-2 border-l-2 border-gray-700">
-                <AddBook showAddBookModal={showAddBookModal} />
+            <section className="mx-auto w-9/12 pt-32 flex-col justify-center items-center">
+                <AddBook showAddBookModal={showCreateForumModal} />
                 <BookList books={books} />
             </section>
 
             <Footer />
 
             <Modal
-                isVisible={isAddBookModalVisible}
-                hideModal={hideAddBookModal}
+                isVisible={isCreateForumModalVisible}
+                hideModal={hideCreateForumModal}
             >
-                <AddBookForm onAddBook={onAddBook} />
+                <CreateForumForm onCreateForum={onCreateForum} />
             </Modal>
 
             <Modal isVisible={isSignInModalVisible} hideModal={hideSignInModal}>
