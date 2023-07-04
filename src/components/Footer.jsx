b952a1f3884696cd9bfdc2e88e7cbd0082cf9c73
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-03 22:06:33 -0400

    Updated styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
 export default function Footer() {
     return (
-        <footer className="w-full my-2.5 p-2.5 border-t-2 border-gray-700 bg-gray-900">
+        <div className="w-11/12 mx-auto p-5 border-t border-gray-300">
             <div
-                className="w-9/12 max-h-8 mx-auto flex justify-center items-center text-xl
+                className="w-9/12 mx-auto flex justify-center items-center text-xl
                 p-2.5"
             >
-                <p className="text-gray-200">
+                <p className="text-midnight">
                     &copy; Copyright 2023 by
                     <a
                         href="https://github.com/PatrickLisiecki"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="ml-1 text-blue-300"
+                        className="ml-1 text-sky-400"
                     >
                         Patrick Lisiecki
                     </a>
                 </p>
             </div>
-        </footer>
+        </div>
     );
 }
