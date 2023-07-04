8aa866db076ca33baafd3449bc8f5f695f96f749
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-03 22:05:45 -0400

    Adjusted logo size and added hover effects

--- src/pages/Homepage/HomepageNavbar.jsx
+++ src/pages/Homepage/HomepageNavbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function HomepageNavbar() {
     return (
-        <div className="w-11/12 mx-auto border-b-2 border-gray-300 top-0">
+        <div className="w-11/12 mx-auto border-b border-gray-300 top-0">
             <div className="w-10/12 p-5 mx-auto flex justify-between">
                 <div className="w-full flex justify-start items-center">
                     <div className="flex flex-row items-center gap-2">
                         <img
-                            className="w-16 h-16"
+                            className="w-12 h-12"
                             src="src/assets/images/darkLogo.svg"
                             alt="Logo"
                         />
@@ -19,26 +19,26 @@ export default function HomepageNavbar() {
                 </div>
                 <div className="w-full">
                     <ul className="w-full flex flex-row justify-end items-center text-xl text-midnight">
-                        <li className="px-10 hover:text-blue-500">
+                        <li className="px-8 hover:text-sky-500">
                             <Link to="/">
                                 <span>Home</span>
                             </Link>
                         </li>
-                        <li className="px-10 hover:text-blue-500">
+                        <li className="px-8 hover:text-sky-500">
                             <Link to="/about">
                                 <span>About</span>
                             </Link>
                         </li>
-                        <li className="px-10 hover:text-blue-500">
+                        <li className="px-8 hover:text-sky-500">
                             <Link to="/contact">
                                 <span>Contact</span>
                             </Link>
                         </li>
-                        <li className="px-10">
+                        <li className="px-8">
                             <Link to="/forums">
                                 <button
                                     type="button"
-                                    className="text-white bg-gray-800 text-base font-semibold rounded-full px-7 py-4 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300"
+                                    className="px-6 py-3 border border-gray-700 hover:border-sky-500 hover:text-sky-500 hover:-translate-y-[2px] focus:outline-none"
                                 >
                                     Go to Forum
                                 </button>
